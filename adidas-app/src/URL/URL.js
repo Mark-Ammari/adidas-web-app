af12abed1a34ec9deb0364ddd28292945eb0d87b
Mark Ammari <mark.ammari@gmail.com>
2020-04-10 14:42:43 -0400

    updated URL to fetch data

--- adidas-app/src/URL/URL.js
+++ adidas-app/src/URL/URL.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+const URL = 'https://adidas-copy.herokuapp.com/' || 'http://localhost:5000';
 
 const base = axios.create({
     baseURL: URL,
