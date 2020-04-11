bc76aea63080f06f44f6fe9b82dc47e02182f3bc
Mark Ammari <mark.ammari@gmail.com>
2020-04-10 23:15:51 -0400

    fixed cors issue

--- adidas-app/src/URL/URL.js
+++ adidas-app/src/URL/URL.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let URL = 'https://adidas-copy.herokuapp.com/' || 'http://localhost:3000';
+let URL = 'https://adidas-copy.herokuapp.com/' && 'http://localhost:3000';
 
 const base = axios.create({
     baseURL: URL,
